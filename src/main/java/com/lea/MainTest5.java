50378bbbb326c50b1d7ebc26cda4da7a37572ac8
Evgenij_Lev <dn160781lea@gmail.com>
2017-01-16 10:05:44 +0200

    тест старт

---
+++ src/main/java/com/lea/MainTest5.java
@@ -0,0 +1,10 @@
+package com.lea;
+
+/**
+ * Created by home on 16.01.17.
+ */
+public class MainTest5 {
+    public static void main(String[] args) {
+        System.out.println("страт, первая модификация");
+    }
+}
